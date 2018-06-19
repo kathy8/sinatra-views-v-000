6beb8d1331c9d6bcdb332a030fdbe46cf77e3dcf
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-06-19 01:53:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,9 +3,10 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		"erb :index"
+		erb :index
 	end
-end
+
 get "/info" do
-	"Testing the info page"
+	erb :info
+end
 end
